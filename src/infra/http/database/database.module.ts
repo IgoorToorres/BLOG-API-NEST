56634494c8d27d9950db56478783928d60467a7c
IgoorToorres <igorevangelista121@gmail.com>
2026-05-06 10:56:28 -0300

    fix - arrumando o database module para usar inversao de dependencia corretamente com os repositories certos

--- src/infra/http/database/database.module.ts
+++ src/infra/http/database/database.module.ts
@@ -9,6 +9,11 @@ import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-
 import { QuestionsRepository } from '@/domain/forum/application/repository/questions-repository'
 import { StudentRepository } from '@/domain/forum/application/repository/student-repository'
 import { PrismaStudentRepository } from './prisma/repositories/prisma-student.repository'
+import { QuestionCommentsRepository } from '@/domain/forum/application/repository/question-comments-repository'
+import { QuestionAttachmentsRepository } from '@/domain/forum/application/repository/question-attachments-repository'
+import { AnswerRepository } from '@/domain/forum/application/repository/answer-repository'
+import { AnswerCommentsRepository } from '@/domain/forum/application/repository/answer-comments-repository'
+import { AnswerAttachmentsRepository } from '@/domain/forum/application/repository/answer-attachments-repository copy'
 
 @Module({
   providers: [
@@ -21,20 +26,36 @@ import { PrismaStudentRepository } from './prisma/repositories/prisma-student.re
       provide: StudentRepository,
       useClass: PrismaStudentRepository,
     },
-    PrismaQuestionCommentsRepository,
-    PrismaQuestionAttachmentsRepository,
-    PrismaAswersRepository,
-    PrismaAswerCommentsRepository,
-    PrismaAnswerAttachmentsRepository,
+
+    {
+      provide: QuestionCommentsRepository,
+      useClass: PrismaQuestionCommentsRepository,
+    },
+    {
+      provide: QuestionAttachmentsRepository,
+      useClass: PrismaQuestionAttachmentsRepository,
+    },
+    {
+      provide: AnswerRepository,
+      useClass: PrismaAswersRepository,
+    },
+    {
+      provide: AnswerCommentsRepository,
+      useClass: PrismaAswerCommentsRepository,
+    },
+    {
+      provide: AnswerAttachmentsRepository,
+      useClass: PrismaAnswerAttachmentsRepository,
+    },
   ],
   exports: [
     PrismaService,
     QuestionsRepository,
-    PrismaQuestionCommentsRepository,
-    PrismaQuestionAttachmentsRepository,
-    PrismaAswersRepository,
-    PrismaAswerCommentsRepository,
-    PrismaAnswerAttachmentsRepository,
+    QuestionCommentsRepository,
+    QuestionAttachmentsRepository,
+    AnswerRepository,
+    AnswerCommentsRepository,
+    AnswerAttachmentsRepository,
     StudentRepository,
   ],
 })
