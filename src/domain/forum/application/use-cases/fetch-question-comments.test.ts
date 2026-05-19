dce394f14a6f5fddb6518db89dca5821ffea8c33
IgoorToorres <igorevangelista121@gmail.com>
2026-05-19 09:34:43 -0300

    feat - value object para retornar comentarios junto com usuario relacionado. (a nivel de dominio)

--- src/domain/forum/application/use-cases/fetch-question-comments.test.ts
+++ src/domain/forum/application/use-cases/fetch-question-comments.test.ts
@@ -3,20 +3,43 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { InMemoryQuestionCommentsRepository } from '@/../test/repositories/in-memory-question-comments-repository'
 import { FetchQuestionCommentsUseCase } from './fetch-question-comments'
 import { makeQuestionComment } from '@/../test/factories/make-question-comment'
+import { InMemoryStudentRepository } from '@/../test/repositories/in-memory-student-repository'
+import { makeStudent } from '@/../test/factories/make-student'
 
 let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let inMemoryStudentRepository: InMemoryStudentRepository
 let sut: FetchQuestionCommentsUseCase
 
 describe('Fetch question comments', () => {
-  beforeEach(() => {
-    inMemoryQuestionCommentsRepository =
-      new InMemoryQuestionCommentsRepository()
+  beforeEach(async () => {
+    inMemoryStudentRepository = new InMemoryStudentRepository()
+    inMemoryQuestionCommentsRepository = new InMemoryQuestionCommentsRepository(
+      inMemoryStudentRepository,
+    )
     sut = new FetchQuestionCommentsUseCase(inMemoryQuestionCommentsRepository)
   })
 
   it('should be able to fetch question comments', async () => {
+    const author1 = await makeStudent(
+      { name: 'John Doe' },
+      new UniqueEntityID('author-1'),
+    )
+    const author2 = await makeStudent(
+      { name: 'Jane Doe' },
+      new UniqueEntityID('author-2'),
+    )
+    const author3 = await makeStudent(
+      { name: 'Paul Doe' },
+      new UniqueEntityID('author-3'),
+    )
+
+    await inMemoryStudentRepository.create(author1)
+    await inMemoryStudentRepository.create(author2)
+    await inMemoryStudentRepository.create(author3)
+
     inMemoryQuestionCommentsRepository.create(
       await makeQuestionComment({
+        authorId: new UniqueEntityID('author-1'),
         questionId: new UniqueEntityID('question-1'),
         createdAt: new Date(2026, 0, 20),
       }),
@@ -24,6 +47,7 @@ describe('Fetch question comments', () => {
 
     inMemoryQuestionCommentsRepository.create(
       await makeQuestionComment({
+        authorId: new UniqueEntityID('author-2'),
         questionId: new UniqueEntityID('question-1'),
         createdAt: new Date(2026, 0, 22),
       }),
@@ -31,6 +55,7 @@ describe('Fetch question comments', () => {
 
     inMemoryQuestionCommentsRepository.create(
       await makeQuestionComment({
+        authorId: new UniqueEntityID('author-3'),
         questionId: new UniqueEntityID('question-1'),
         createdAt: new Date(2026, 0, 18),
       }),
@@ -41,17 +66,34 @@ describe('Fetch question comments', () => {
       questionId: 'question-1',
     })
 
-    expect(result.value?.questionComments).toEqual([
-      expect.objectContaining({ createdAt: new Date(2026, 0, 22) }),
-      expect.objectContaining({ createdAt: new Date(2026, 0, 20) }),
-      expect.objectContaining({ createdAt: new Date(2026, 0, 18) }),
+    expect(result.value?.comments).toEqual([
+      expect.objectContaining({
+        createdAt: new Date(2026, 0, 22),
+        author: 'Jane Doe',
+      }),
+      expect.objectContaining({
+        createdAt: new Date(2026, 0, 20),
+        author: 'John Doe',
+      }),
+      expect.objectContaining({
+        createdAt: new Date(2026, 0, 18),
+        author: 'Paul Doe',
+      }),
     ])
   })
 
   it('should be able to fetch pagination question comments', async () => {
+    const author = await makeStudent(
+      { name: 'John Doe' },
+      new UniqueEntityID('author-1'),
+    )
+
+    await inMemoryStudentRepository.create(author)
+
     for (let i = 1; i <= 22; i++) {
       inMemoryQuestionCommentsRepository.create(
         await makeQuestionComment({
+          authorId: new UniqueEntityID('author-1'),
           questionId: new UniqueEntityID('question-1'),
         }),
       )
@@ -62,6 +104,6 @@ describe('Fetch question comments', () => {
       questionId: 'question-1',
     })
 
-    expect(result.value?.questionComments).toHaveLength(2)
+    expect(result.value?.comments).toHaveLength(2)
   })
 })
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -2,9 +2,13 @@ import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionCommentsRepository } from '@/domain/forum/application/repository/question-comments-repository'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+import { InMemoryStudentRepository } from './in-memory-student-repository'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
 
 export class InMemoryQuestionCommentsRepository implements QuestionCommentsRepository {
   public items: QuestionComment[] = []
+
+  constructor(private studentRepository: InMemoryStudentRepository) {}
 
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment)
@@ -33,6 +37,38 @@ export class InMemoryQuestionCommentsRepository implements QuestionCommentsRepos
       .filter((item) => item.questionId.toString() === questionId)
       .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
       .slice((page - 1) * 20, page * 20)
+
+    return questionComments
+  }
+
+  async findManyByQuestionIdWithAuthor(
+    questionId: string,
+    { page }: PaginationParams,
+  ) {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        const author = this.studentRepository.items.find((item) =>
+          item.id.equals(comment.authorId),
+        )
+
+        if (!author) {
+          throw new Error(
+            `Author with ID ${comment.authorId.toString()} does not exists`,
+          )
+        }
+
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          content: comment.content,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+          authorId: comment.authorId,
+          author: author.name,
+        })
+      })
 
     return questionComments
   }
